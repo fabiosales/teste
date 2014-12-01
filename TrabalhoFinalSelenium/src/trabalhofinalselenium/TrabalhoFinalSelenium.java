e71c07ead8b5edb44e68747ceb7b7ab38a831028
WIN8 <WIN8@FABIO>
2014-12-01 20:16:00 -0200

    Teste Primeiro Comittt

--- TrabalhoFinalSelenium/src/trabalhofinalselenium/TrabalhoFinalSelenium.java
+++ TrabalhoFinalSelenium/src/trabalhofinalselenium/TrabalhoFinalSelenium.java
@@ -14,6 +14,7 @@ public class TrabalhoFinalSelenium {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         //abre firefox
         WebDriver driver = new FirefoxDriver();
         
